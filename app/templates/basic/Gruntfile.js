14afe03c5949b2d4b6dbd77da054d80931e33acf
Oscar Tong <moscartong@gmail.com>
2014-06-26 20:40:33 +0800

    update grunt and node config files

--- app/templates/basic/Gruntfile.js
+++ app/templates/basic/Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
     develop: {
       server: {
-        file: 'app.js'
+        file: './bin/www'
       }
     },
     watch: {

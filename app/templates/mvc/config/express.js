300c67f0599d06a911605d2e95e76a4d86b0bb4a
knu2xs <knu2xs@gmail.com>
2014-08-10 11:03:37 -0400

    add cookieparser back into mvc/config/express.js

--- app/templates/mvc/config/express.js
+++ app/templates/mvc/config/express.js
@@ -19,6 +19,7 @@ module.exports = function(app, config) {
   app.use(bodyParser.urlencoded({
     extended: true
   }));
+  app.use(cookieParser());
   app.use(compress());
   app.use(express.static(config.root + '/public'));
   app.use(methodOverride());

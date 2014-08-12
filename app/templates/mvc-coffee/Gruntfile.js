7d5eba79faa70e75f3fd3ce7e476d5ecba796c0c
fritz-c <us.chrisf@gmail.com>
2014-08-13 01:27:09 +0900

    Use js Gruntfile for compatibility
    Allow grunt execution on servers without CoffeeScript installed,
    by leaving frontmost files as js, which then use the coffee binary
    in the dependencies.

---
+++ app/templates/mvc-coffee/Gruntfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var request = require('request');
+
+module.exports = function (grunt) {
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  var reloadPort = 35729, files;
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    develop: {
+      server: {
+        file: 'app.js'
+      }
+    },
+    watch: {
+      options: {
+        nospawn: true,
+        livereload: reloadPort
+      },
+      js: {
+        files: [
+          'app.coffee',
+          'app/**/*.coffee',
+          'config/*.coffee'
+        ],
+        tasks: ['develop', 'delayed-livereload']
+      },
+      views: {
+        files: [
+          'app/views/*.<%= options.viewEngine %>',
+          'app/views/**/*.<%= options.viewEngine %>'
+        ],
+        options: { livereload: reloadPort }
+      }
+    }
+  });
+
+  grunt.config.requires('watch.js.files');
+  files = grunt.config('watch.js.files');
+  files = grunt.file.expand(files);
+
+  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
+    var done = this.async();
+    setTimeout(function () {
+      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function(err, res) {
+          var reloaded = !err && res.statusCode === 200;
+          if (reloaded)
+            grunt.log.ok('Delayed live reload successful.');
+          else
+            grunt.log.error('Unable to make a delayed live reload.');
+          done(reloaded);
+        });
+    }, 500);
+  });
+
+  grunt.registerTask('default', ['develop', 'watch']);
+};

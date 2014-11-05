872d6e4149a90bd9d5002aad9dc0756555e50c49
Yaacov Zamir <kobi.zamir@gmail.com>
2014-11-05 22:52:33 +0200

    add gulp sass and node-sass support

--- app/templates/extras/basic-coffee/gulpfile.js
+++ app/templates/extras/basic-coffee/gulpfile.js
@@ -1,6 +1,29 @@
 var gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
-  livereload = require('gulp-livereload');
+  livereload = require('gulp-livereload');<% if(options.cssPreprocessor == 'sass'){ %>
+  var sass = require('gulp-ruby-sass');<% } %><% if(options.cssPreprocessor == 'node-sass'){ %>
+  var sass = require('gulp-sass');<% } %>
+<% if(options.cssPreprocessor == 'sass'){ %>
+gulp.task('sass', function () {
+    return gulp.src('./public/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/css'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./public/css/*.scss', ['sass']);
+});<% } %><% if(options.cssPreprocessor == 'node-sass'){ %>
+gulp.task('sass', function () {
+    gulp.src('./public/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/css'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./public/css/*.scss', ['sass']);
+});<% } %>
 
 gulp.task('develop', function () {
   livereload.listen();
@@ -14,4 +37,8 @@ gulp.task('develop', function () {
   });
 });
 
-gulp.task('default', ['develop']);
+gulp.task('default', [<% if(options.cssPreprocessor == 'sass' || options.cssPreprocessor == 'node-sass'){ %>
+  'sass',<% } %>
+  'develop'<% if(options.cssPreprocessor == 'sass' || options.cssPreprocessor == 'node-sass'){ %>,
+  'watch'<% } %>
+]);

ecfcdcaf0645d7659f34b64842b14ef1a730170a
petecoop <pete@petecoop.co.uk>
2015-08-12 00:03:22 +0100

    gulp replace timeout reload #128

--- app/templates/extras/mvc-shared/gulpfile.js
+++ app/templates/extras/mvc-shared/gulpfile.js
@@ -55,10 +55,15 @@ gulp.task('develop', function () {
   nodemon({
     script: 'app.js',
     ext: 'js coffee <%= options.viewEngine %>',
-  }).on('restart', function () {
-    setTimeout(function () {
-      livereload.changed(__dirname);
-    }, 500);
+    stdout: false
+  }).on('readable', function () {
+    this.stdout.on('data', function (chunk) {
+      if(/^Express server listening on port/.test(chunk)){
+        livereload.changed(__dirname);
+      }
+    });
+    this.stdout.pipe(process.stdout);
+    this.stderr.pipe(process.stderr);
   });
 });
 
--- app/templates/mvc-shared/app.js
+++ app/templates/mvc-shared/app.js
@@ -25,10 +25,14 @@ require('./config/express')(app, config);
 db.sequelize
   .sync()
   .then(function () {
-    app.listen(config.port);
+    app.listen(config.port, function () {
+      console.log('Express server listening on port ' + config.port);
+    });
   }).catch(function (e) {
     throw new Error(e);
   });
 <% } else { %>
-app.listen(config.port);
+app.listen(config.port, function () {
+  console.log('Express server listening on port ' + config.port);
+});
 <% } %>
